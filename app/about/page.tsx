68f4afea86090c306c3628143426eac0081abf3a
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-05-05 23:24:50 +0630

    about-page-done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,38 +1,48 @@
 import DownloadResume from "@/components/about/DownloadResume";
-import { Button } from "@/components/ui/button";
+import { db } from "@/server";
 import { auth } from "@/server/auth";
 import { Pencil } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const AboutPage = async () => {
   const session = await auth();
   const role = session?.user.role;
+  const aboutData = await db.query.about.findFirst();
+  console.log("I am about data", aboutData);
   return (
-    <div>
-      <div>
-        <div className=" flex gap-2 items-center">
-          <h1>About</h1>
-          {role === "admin" && (
-            <>
-              <Link href={"/dashboard/about-edit"}>
-                <Pencil className=" size-4 text-purple-600" />
-              </Link>
-            </>
+    <div className=" h-screen flex items-start mt-2">
+      <div className=" flex justify-between w-full items-end gap-2">
+        <div className=" w-[800px] space-y-4">
+          <div className=" flex gap-2 items-center ">
+            <h1 className=" flex items-center gap-4">
+              <span className=" text-4xl font-bold ">About</span>
+              {role === "admin" && (
+                <>
+                  <Link href={"/dashboard/about-edit"}>
+                    <Pencil className=" size-4 text-purple-600" />
+                  </Link>
+                </>
+              )}
+            </h1>
+          </div>
+          <hr />
+          <p>{aboutData?.aboutMe}</p>
+          <DownloadResume />
+        </div>
+        <div className=" w-[400px] h-[600px] object-cover overflow-hidden  rounded-bl-4xl rounded-tr-4xl">
+          {aboutData?.image && (
+            <Image
+              src={aboutData.image}
+              alt="img"
+              width={600} // အသုံးပြုမဲ့ UI အတိုင်းသတ်မှတ်
+              height={400}
+              className=" w-full h-full object-cover"
+            />
           )}
         </div>
-        <hr />
-        <p>
-          Hey, my name is Charles Bruyerre and I use Sharlee as my nickname
-          across social medias. I’m a graphic designer, UX/UI designer &
-          front-end web developer from France. I’m also passionate about pop
-          music and make portraits and universes around what I listen to and I’m
-          always curious to learn more when it comes to new technologies and
-          creative coding.
-        </p>
       </div>
-      <DownloadResume />
-      {/* <img src="" alt="" /> */}
     </div>
   );
 };
--- components/about/DownloadResume.tsx
+++ components/about/DownloadResume.tsx
@@ -1,16 +1,17 @@
 import { Download } from "lucide-react"; // optional icon
 import { Button } from "@/components/ui/button"; // သင့် UI Button component ဖြစ်နိုင်
+import Link from "next/link";
 
 const DownloadResume = () => {
   return (
-    <a
+    <Link
       href="/Ye_Htet_Aung_Frontend_Developer.pdf"
       download
-      className="inline-flex items-center px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary/90 transition"
+      className="inline-flex items-center  underline rounded-lg hover:bg-primary/90 transition text-purple-600"
     >
       <Download className="w-4 h-4 mr-2" />
       Download Resume
-    </a>
+    </Link>
   );
 };
 
