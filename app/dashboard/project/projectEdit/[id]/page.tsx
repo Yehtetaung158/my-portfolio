a1b3b427b04cc86ff564e3769bff92a9b6b72885
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-05-09 22:42:40 +0630

    5

--- app/dashboard/project/projectEdit/[id]/page.tsx
+++ app/dashboard/project/projectEdit/[id]/page.tsx
@@ -1,4 +1,3 @@
-import { GetServerSideProps } from "next";
 import ProjectEditForm from "@/components/project/projectEditForm";
 import { getProjectById } from "@/server/action/projectAction";
 
@@ -6,28 +5,10 @@ interface ProjectEditProps {
   params: { id: string };
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const projectId = parseInt(context.params?.id as string);
+export default async function ProjectEdit({ params }: ProjectEditProps) {
+  const projectId = parseInt(params.id);
   const project = await getProjectById(projectId);
 
-  if (!project) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: {
-      params: { id: context.params?.id },
-      project,
-    },
-  };
-};
-
-export default function ProjectEdit({
-  params,
-  project,
-}: ProjectEditProps & { project: any }) {
   if (!project) {
     return <div className="text-red-500">Project not found</div>;
   }
